5d4e79d95591ef4af84be7ce7369b3d72807ae9d
3xo13 <aa335@students.kiron.ngo>
2023-09-12 19:42:33 +0300

    changed the reciving emails

--- app/api/submit/route.js
+++ app/api/submit/route.js
@@ -9,9 +9,9 @@ async function mail(name, email, phone, items, message) {
     const plainTextMsg = `Name: ${name}\nEmail: ${email}\nPhone: ${phone} \nItems: ${items}\nMessage: ${message}`;
     const htmlMsg = createTemplate(name, email, phone, items, message);
     // Generate test SMTP service account from ethereal.email Only needed if you
-    // don't have a real mail account for testing let testAccount = await
+    // don't have a real mail account for testing let senderAccount = await
     // nodemailer.createTestAccount();
-    let testAccount = {
+    let senderAccount = {
         user: process.env.USER_EMAIL,
         pass: process.env.USER_PASSWORD
     }
@@ -20,15 +20,15 @@ async function mail(name, email, phone, items, message) {
     let transporter = nodemailer.createTransport({
         host: "mail.alpha-limit.com", port: 465, secure: true, // true for 465, false for other ports
         auth: {
-            user: testAccount.user, // generated ethereal user
-            pass: testAccount.pass, // generated ethereal password
+            user: senderAccount.user, // generated ethereal user
+            pass: senderAccount.pass, // generated ethereal password
         }
     });
 
     // send mail with defined transport object
     let info = await transporter.sendMail({
         from: `"${name}" <new-quote@alpha-limit.com>`, // sender address
-        to: "alkarim1310@gmail.com", // list of receivers
+        to: "sales@alpha-limit.com + support@alpha-limit.com", // list of receivers
         subject: "New Request Inquiry ✔", // Subject line
         text: plainTextMsg, // plain text body
         html: htmlMsg, // html body
