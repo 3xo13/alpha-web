242265f60b3f689647c85fadbfcf25a9ecaff7ac
3xo13 <aa335@students.kiron.ngo>
2023-10-03 03:35:48 +0300

    rerouted subCategories from the mega menu (from category to categories page)

--- components/navigation/MegaMenuCategory.jsx
+++ components/navigation/MegaMenuCategory.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import {v4 as uuidv4} from 'uuid';
 
 const MegaMenuCategory = ({category}) => {
-    console.log("🚀 category:", category)
+
     const cat = encodeURIComponent(category.name)
   return (
     <div key={uuidv4()} className='cinzel text-xl'>
@@ -24,7 +24,7 @@ const MegaMenuCategory = ({category}) => {
             .map(sub => {
                 const encodedSub = encodeURIComponent(encodeURIComponent(sub.name))
                 return (
-                    <Link key={uuidv4()} href={`/category/${cat}/${encodedSub}`}>
+                    <Link key={uuidv4()} href={`/categories/${cat}/${encodedSub}`}>
                         <div className="flex flex-row w-full items-center" key={uuidv4()}>
                             <h4 className=" capitalize text-lg">
                                 {
