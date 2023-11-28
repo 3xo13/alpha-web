16613c2e2c0e6fb2a5dede7e3fbd4c258254bc0a
3xo13 <aa335@students.kiron.ngo>
2023-11-28 22:28:48 +0300

    changed the fetch statment in the search page

--- app/search/[query]/page.jsx
+++ app/search/[query]/page.jsx
@@ -5,23 +5,24 @@ import {v4 as uuidv4} from 'uuid';
 
 function searchquery({params}) {
     const [products, setProducts] = useState([]);
-    const [loading,setLoading] = useState(true)
+    const [loading, setLoading] = useState(true)
 
     const fetchHandler = async () => {
         try {
-            
+
             const {query} = params;
             const res = await fetch(`/api/query`, {
                 method: 'POST',
                 body: JSON.stringify({query: query})
             })
             const data = await res.json()
-            setLoading(false)
-            if (!res.ok) 
+            
+            if (!res.ok) {
                 throw new Error(data.error || 'Something went wrong')
-            setProducts(
-                data.products.map(item => <ProductCard key={uuidv4()} product={item}/>)
-            )
+            }
+
+            setProducts(data.products)
+            setLoading(false)
         } catch (e) {
             console.log(e.message)
             setLoading(false)
@@ -32,22 +33,27 @@ function searchquery({params}) {
         fetchHandler()
     }, [])
 
+    const productsFound = products && products.map(
+        item => <ProductCard key={uuidv4()} product={item}/>
+    )
+
     return (
         <div
             className="w-screen h-fit min-h-120 flex flex-row flex-wrap items-center justify-center mt-44">
-                {
+            {
                 loading && <div className="flex flex-row-center screen">
                         <p className="text-6xl mr-10 text-yellow-500">Loading...</p>
                         <img
                             src="/assets/icons/loading.png"
                             alt="lodaing"
-                            className="w-16 h-16 animate-spin"/> 
+                            className="w-16 h-16 animate-spin"/>
                     </div>
             }
             {
-                !products?.length && !loading
+                !products
+                    ?.length && !loading
                         ? (<p className="pt-10 h-[50vh] text-2xl text-orange-700">No Products Found</p>)
-                        : products
+                        : productsFound
             }
         </div>
     )
