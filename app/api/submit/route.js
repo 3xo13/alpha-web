a76a7d642ea9ee2f6d7dc472cfb1aaf3f7ca0f60
3xo13 <aa335@students.kiron.ngo>
2024-01-08 14:25:15 +0300

    testing form submission (2)

--- app/api/submit/route.js
+++ app/api/submit/route.js
@@ -5,34 +5,35 @@ import createTemplate from '@/functions/template';
 
 // async..await is not allowed in global scope, must use a wrapper
 async function mail(name, email, phone, items, message) {
-
-    const plainTextMsg = `Name: ${name}\nEmail: ${email}\nPhone: ${phone} \nItems: ${items}\nMessage: ${message}`;
-    const htmlMsg = createTemplate(name, email, phone, items, message);
-    // Generate test SMTP service account from ethereal.email Only needed if you
-    // don't have a real mail account for testing let senderAccount = await
-    // nodemailer.createTestAccount();
-    let senderAccount = {
-        user: process.env.USER_EMAIL,
-        pass: process.env.USER_PASSWORD
-    }
-
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: "mail.alpha-limit.com", port: 465, secure: true, // true for 465, false for other ports
-        auth: {
-            user: senderAccount.user, // generated ethereal user
-            pass: senderAccount.pass, // generated ethereal password
+        const plainTextMsg = `Name: ${name}\nEmail: ${email}\nPhone: ${phone} \nItems: ${items}\nMessage: ${message}`;
+        const htmlMsg = createTemplate(name, email, phone, items, message);
+        // Generate test SMTP service account from ethereal.email Only needed if you
+        // don't have a real mail account for testing let senderAccount = await
+        // nodemailer.createTestAccount();
+        let senderAccount = {
+            user: process.env.USER_EMAIL,
+            pass: process.env.USER_PASSWORD
         }
-    });
     
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-        from: `"${name}" <new-quote@alpha-limit.com>`, // sender address
-        to: "sales@alpha-limit.com + support@alpha-limit.com + hades131090@gmail.com", // list of receivers
-        subject: "New Request Inquiry ✔", // Subject line
-        text: plainTextMsg, // plain text body
-        html: htmlMsg, // html body
-    });
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            host: "mail.alpha-limit.com", port: 465, secure: true, // true for 465, false for other ports
+            auth: {
+                user: senderAccount.user, // generated ethereal user
+                pass: senderAccount.pass, // generated ethereal password
+            }
+        });
+        
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+            from: `"${name}" <new-quote@alpha-limit.com>`, // sender address
+            to: "sales@alpha-limit.com + support@alpha-limit.com + hades131090@gmail.com", // list of receivers
+            subject: "New Request Inquiry ✔", // Subject line
+            text: plainTextMsg, // plain text body
+            html: htmlMsg, // html body
+        });
+
+
 
 }
 
@@ -45,7 +46,7 @@ export const POST = async (request) => {
 
         await newQuote.save();
 
-        mail(name, email, phone, items, message).catch(err => console.error(err));
+        mail(name, email, phone, items, message).catch(err => console.error(`Error occured while sending your email, Error:${err}`));
 
         return new Response(JSON.stringify(newQuote), {status: 201})
     } catch (error) {
